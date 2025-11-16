953fb6bfafa11aa1fd58a377a7bb9a32b5f48a0c
ima-aarr <s103765s103765@gmail.com>
2025-11-17 08:36:39 +0900

    Update duel.py

--- commands/duel.py
+++ commands/duel.py
@@ -1,16 +1,24 @@
 from discord.ext import commands
 import json, random
 
+DATA_FILE = "data/players.json"
+
 @commands.command()
 async def duel(ctx, target: commands.MemberConverter):
-    with open("data/players.json", "r") as f:
+    with open(DATA_FILE, "r") as f:
         players = json.load(f)
 
-    user_id = str(ctx.author.id)
-    target_id = str(target.id)
-    if user_id not in players or target_id not in players:
-        await ctx.send("両方のプレイヤーがキャラクターを作成している必要があります。")
+    a = str(ctx.author.id)
+    b = str(target.id)
+    if a not in players or b not in players:
+        await ctx.send("両方がキャラ作成してへんとあかんで。")
         return
 
-    winner = random.choice([ctx.author.name, target.name])
-    await ctx.send(f"{ctx.author.name} と {target.name} の決闘の勝者は {winner} です！")
+    # simple duel: compare level + random
+    la = players[a].get("level",1) + random.randint(0,3)
+    lb = players[b].get("level",1) + random.randint(0,3)
+    if la >= lb:
+        winner = ctx.author.name
+    else:
+        winner = target.name
+    await ctx.send(f"{ctx.author.name} と {target.name} の決闘！勝者は **{winner}** や！")
