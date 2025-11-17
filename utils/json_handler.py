325288a5f5881e3c539d3378bb4ef7ba518cd29a
ima-aarr <s103765s103765@gmail.com>
2025-11-17 12:41:49 +0900

    Update json_handler.py

--- utils/json_handler.py
+++ utils/json_handler.py
@@ -3,16 +3,13 @@ import os
 
 DB_PATH = "db/db.json"
 
-# データ読み込み
 def load_db():
     if not os.path.exists(DB_PATH):
-        # DBがなければ初期化
         with open(DB_PATH, "w") as f:
             json.dump({"characters":[],"items":[],"quests":[],"countries":[]}, f, indent=4)
     with open(DB_PATH, "r") as f:
         return json.load(f)
 
-# データ保存
 def save_db(data):
     with open(DB_PATH, "w") as f:
         json.dump(data, f, indent=4)
