997c0f115666fd8926bb7690e0bff6a05667f1e0
ima-aarr <s103765s103765@gmail.com>
2025-11-17 12:23:41 +0900

    Update party.py

--- commands/party.py
+++ commands/party.py
@@ -1,26 +1,5 @@
 from discord.ext import commands
-import json, os
-
-DATA_FILE = "data/players.json"
 
 @commands.command()
-async def party(ctx, target: commands.MemberConverter):
-    with open(DATA_FILE, "r") as f:
-        players = json.load(f)
-
-    a = str(ctx.author.id)
-    b = str(target.id)
-    if a not in players or b not in players:
-        await ctx.send("両方がキャラ作成してへんとあかんで。")
-        return
-
-    players[a].setdefault("party", [])
-    if b in players[a]["party"]:
-        await ctx.send(f"{target.name} は既にパーティーに入ってるで。")
-        return
-
-    players[a]["party"].append(b)
-    with open(DATA_FILE, "w") as f:
-        json.dump(players, f, indent=2, ensure_ascii=False)
-
-    await ctx.send(f"{ctx.author.name} と {target.name} がパーティーを組んだで。")
+async def party(ctx):
+    await ctx.send("パーティ情報を表示します。")
