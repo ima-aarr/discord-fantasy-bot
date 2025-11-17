a1396651a5de6c26eefdbded8e781f3fc79ea02d
ima-aarr <s103765s103765@gmail.com>
2025-11-17 12:22:35 +0900

    Update act.py

--- commands/act.py
+++ commands/act.py
@@ -1,19 +1,5 @@
 from discord.ext import commands
-import json, os
-from llm.llm_model import generate
-
-DATA_FILE = "data/players.json"
 
 @commands.command()
-async def act(ctx, *, action):
-    with open(DATA_FILE, "r") as f:
-        players = json.load(f)
-
-    uid = str(ctx.author.id)
-    if uid not in players:
-        await ctx.send("まず /create_character をしてや。")
-        return
-
-    prompt = f"{players[uid]['player_name']} が {action} を行った結果をRPG風に生成してください。"
-    result = generate(prompt)
-    await ctx.send(result)
+async def act(ctx, action: str):
+    await ctx.send(f"{action} の行動を実行しました！")
