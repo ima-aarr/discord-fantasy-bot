d5bd40457ef86a1cda7a633e7f032b3861056349
ima-aarr <s103765s103765@gmail.com>
2025-11-17 12:14:53 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-set -e
-
-# make sure data dir exists
-mkdir -p data
-
-# Upgrade pip then install requirements (Koyeb will cache)
-pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt
-
-# Start bot
-python bot.py
+python3 bot.py
