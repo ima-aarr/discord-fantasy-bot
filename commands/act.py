e7808fda712d48352b59613033459fc9255aaf2f
ima-aarr <s103765s103765@gmail.com>
2025-11-17 15:34:53 +0900

    Enhance act command to track actions in database
    Load character data from the database and save actions taken.

--- commands/act.py
+++ commands/act.py
@@ -1,5 +1,13 @@
 from discord.ext import commands
+from utils.json_handler import load_db, save_db
 
 @commands.command()
-async def act(ctx, action: str):
-    await ctx.send(f"{action} の行動を実行しました！")
+async def act(ctx, *, action: str):
+    db = load_db()
+    for c in db["characters"]:
+        if c["user_id"] == str(ctx.author.id):
+            c["actions_taken"].append(action)
+            save_db(db)
+            await ctx.send(f"{c['name']} は '{action}' を行いました。")
+            return
+    await ctx.send("キャラクターが存在しません。")
