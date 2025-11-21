203d2b858147bfe967e01e1b77d0de288930094e
ima-aarr <s103765s103765@gmail.com>
2025-11-21 20:27:58 +0900

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -1,14 +1,19 @@
 import os
 import json
+import base64
 import firebase_admin
 from firebase_admin import credentials, db
-import base64
 
-service_account_info = json.loads(
-    base64.b64decode(os.environ["FIREBASE_SERVICE_ACCOUNT_B64"])
-)
+# 環境変数からBase64文字列を取得
+service_account_b64 = os.environ.get("FIREBASE_SERVICE_ACCOUNT_B64")
+service_account_json = base64.b64decode(service_account_b64)
 
-cred = credentials.Certificate(service_account_info)
+with open("serviceAccountKey.json", "wb") as f:
+    f.write(service_account_json)
+
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    "databaseURL": os.environ["FIREBASE_DATABASE_URL"]
+    'databaseURL': os.environ.get("FIREBASE_DB_URL")
 })
+
+db_ref = db.reference("/")
