fc3aa0cf7900f1974ea7aa56929d505c093b1332
ima-aarr <s103765s103765@gmail.com>
2025-11-22 10:39:04 +0900

    Update trade.py

--- commands/trade.py
+++ commands/trade.py
@@ -1,26 +1,8 @@
 from discord.ext import commands
-from utils.storage import load_data, save_data
-from utils.llm import generate_text
-
-@commands.command()
-async def trade(ctx, target: commands.MemberConverter, resource: str, amount: int):
-    data = load_data()
+from utils.storage import push_event
+@commands.command(name="trade")
+async def trade(ctx, member: commands.MemberConverter, *, item: str):
     uid = str(ctx.author.id)
-    tid = str(target.id)
-    user = data.get("players", {}).get(uid)
-    partner = data.get("players", {}).get(tid)
-    if not user or not partner:
-        await ctx.send("両方のプレイヤーがキャラクターを作成する必要があります。")
-        return
-    user.setdefault("resources", {})
-    partner.setdefault("resources", {})
-    user["resources"][resource] = user["resources"].get(resource, 0) - amount
-    partner["resources"][resource] = partner["resources"].get(resource, 0) + amount
-    user.setdefault("trade", []).append({"to": tid, "resource": resource, "amount": amount})
-    partner.setdefault("trade", []).append({"from": uid, "resource": resource, "amount": amount})
-    user.setdefault("actions_taken", []).append(f"trade {amount} {resource} to {partner.get('character_name','匿名')}")
-    partner.setdefault("actions_taken", []).append(f"receive {amount} {resource} from {user.get('character_name','匿名')}")
-    prompt = f"{user.get('country_name','国名不明')} が {partner.get('country_name','国名不明')} に {resource} {amount} を交易しました。交易記録をゲーム世界観で書いてください。"
-    message = generate_text(prompt)
-    save_data(data)
-    await ctx.send(message)
+    target = str(member.id)
+    push_event({"type":"trade","from":uid,"to":target,"item":item})
+    await ctx.send(f"{ctx.author.mention} {member.mention} に取引を提案したで。アイテム: {item}")
