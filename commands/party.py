5896f0f1cef59956895f61aeda48225b78aacb32
ima-aarr <s103765s103765@gmail.com>
2025-11-22 21:24:50 +0900

    Delete commands directory

--- commands/party.py
+++
@@ -1,8 +0,0 @@
-from discord.ext import commands
-from utils.storage import push_event
-@commands.command(name="party")
-async def party(ctx, member: commands.MemberConverter):
-    uid = str(ctx.author.id)
-    target = str(member.id)
-    push_event({"type":"party","from":uid,"to":target})
-    await ctx.send(f"{ctx.author.mention} {member.mention} にパーティ招待を送ったで。")
--- commands/trade.py
+++
@@ -1,8 +0,0 @@
-from discord.ext import commands
-from utils.storage import push_event
-@commands.command(name="trade")
-async def trade(ctx, member: commands.MemberConverter, *, item: str):
-    uid = str(ctx.author.id)
-    target = str(member.id)
-    push_event({"type":"trade","from":uid,"to":target,"item":item})
-    await ctx.send(f"{ctx.author.mention} {member.mention} に取引を提案したで。アイテム: {item}")
