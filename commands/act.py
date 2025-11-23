beff2e0d13fee0a32b74a3dcd6381681809e734b
ima-aarr <s103765s103765@gmail.com>
2025-11-23 20:14:22 +0900

    Delete commands directory

--- commands/act.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-from systems.world import move_player_generate_event
-
-async def setup_act_cmd(bot):
-
-    @bot.command(name="act")
-    async def act(ctx, *, action):
-        result = await move_player_generate_event(ctx.author.id, action)
-        await ctx.reply(result)
